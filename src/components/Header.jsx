4f0f453216c49a52c3de402ad5e32e5281c18018
dmacisso <dmacisso@gmail.com>
2024-11-24 13:18:09 -0500

    Fixed bug on landing page display the number of open tasks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,14 @@
 /* eslint-disable react/prop-types */
 export function Header({ todos }) {
-  // const { todos } = props;
-  const todosLength = todos.length;
-  const isTasksPlural = todosLength !== 1;
-
+  const openTasks = (todos.filter((todo) => !todo.complete)).length;
+  const isTasksPlural = openTasks !== 1; 
   const taskOrTasks = isTasksPlural ? 'tasks' : 'task';
+
 
   return (
     <header>
       <h1 className="text-gradient">
-        You have {todosLength} open {taskOrTasks}.
+        You have {openTasks} open {taskOrTasks}.
       </h1>
     </header>
   );
